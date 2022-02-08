d899319af5ca4c1b6146b724e6111bdf56f12435
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-02-08 14:48:27 +0200

    added logic to display all todos on home page

---
+++ todos-api/public/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $.getJSON('/api/todos')
+    .then(addTodos)
+})
+
+function addTodos(todos) {
+    todos.forEach((todo) =>{
+        let newTodo = $('<li class="task">'+todo.name+'</li>')
+        if(todo.completed){
+            newTodo.addClass('done')
+        }
+        $('.list').append(newTodo)
+    })
+}
